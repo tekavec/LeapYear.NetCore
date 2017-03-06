a4e559bd9f3ac90ce7698423848946cd6b5ea9bc
tekavec <tomaz@codurance.com>
2017-03-06 07:43:46 +0000

    Add negative tes; refactor to use xUnit Theory

--- LeapYear.NetCore.Tests/LeapYearShould.cs
+++ LeapYear.NetCore.Tests/LeapYearShould.cs
@@ -5,12 +5,14 @@ namespace LeapYear.NetCore.Tests
 {
     public class LeapYearShould
     {
-        [Fact]
-        public void be_a_leap_year_if_divisible_by_4()
+        [Theory]
+        [InlineData(1996, true)]
+        [InlineData(1997, false)]
+        public void be_a_leap_year_if_divisible_by_4(int value, bool expected)
         {
-            var year = new Year(1996);
+            var year = new Year(value);
 
-            year.IsLeapYear().Should().Be(true);
+            year.IsLeapYear().Should().Be(expected);
         }
     }
 }
